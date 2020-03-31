0059ccae341017bbdcd41e269d6bfb28ebe1c5a5
Erwan <1104706-erwan44115@users.noreply.gitlab.com>
2020-03-31 15:41:17 +0200

    comments

--- script_channel.py
+++ script_channel.py
@@ -2,13 +2,13 @@ from apiclient.discovery import build
 import pymongo
 import json
 
-api_key = "AIzaSyCJPH-OV7sq1MJM8_8rJhlDKMBP--dHQuo" # clé de l'api youtube
+api_key = YOUR API KEY HERE # clé de l'api youtube
 
 youtube = build('youtube','v3',developerKey=api_key)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["videos"]
+mydb = myclient["mydatabase"] # the database
+mycol = mydb["videos"] # the collection
 
 
 listChannelId = mycol.find({}, {'channelId':1, '_id':0}) # requete de toute les chaines étant actuellement dans la bd
@@ -24,18 +24,3 @@ for id in listChannelId:
         newChaineAttribut = {"$set": {"name": response['items'][0]['snippet']['title'], "description": response['items'][0]['snippet']['description']}}
         #INSERER ICI DANS LA BASE NOSQL LES INFOS
         mycol.update_one({'channelId': id['channelId']}, newChaineAttribut)
-
-
-
-
-
-# request = youtube.channels().list(
-#     part="snippet,contentDetails,statistics",
-#     id="UCY-_QmcW09PHAImgVnKxU2g"
-# )
-#
-# response = request.execute()
-#print(response['items'][0]['snippet']['title'])
-#print(response['items'][0]['snippet']['description'])
-
-# print(response)
--- script_commentaire.py
+++ script_commentaire.py
@@ -2,33 +2,33 @@ from apiclient.discovery import build
 import pymongo
 import json
 
-api_key = "AIzaSyCJPH-OV7sq1MJM8_8rJhlDKMBP--dHQuo" # clé de l'api youtube
+api_key = YOUR API KEY HERE # clé de l'api youtube
 
 youtube = build('youtube','v3',developerKey=api_key)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["videos"]
+mydb = myclient["mydatabase"] #the database
+mycol = mydb["videos"] # the collection
 
-listChannel = mycol.find()
+listChannel = mycol.find() # list of all channel with all attributes
 
-listChannelId = mycol.find({}, {'channelId':1, '_id':0})
+listChannelId = mycol.find({}, {'channelId':1, '_id':0}) # list of all channel ID
 
 for channel in listChannel:
-    for video in channel['video'][0:1]:
+    for video in channel['video']: # for all vidéos in the channel
         request = youtube.commentThreads().list(
             part="snippet,replies",
             maxResults=10,
             videoId=video['videoId']
-        )
+        ) # request of 10 first comments of the vidéo
         try:
             response = request.execute()
         except Exception as e:
-            raise
-        
+            print(e)
+
         commentList = []
-        if response['items']:
-            for comment in response['items']:
+        if response['items']: # if a video have comments
+            for comment in response['items']: # we put all comments in a list
 
                 newCommentaire = {}
 
@@ -38,5 +38,5 @@ for channel in listChannel:
                 newCommentaire['publishedAt'] = comment['snippet']['topLevelComment']['snippet']['publishedAt']
                 commentList.append(newCommentaire)
 
-        mycol.update_one({'video':{'$elemMatch':{'videoId': video['videoId']}}},{'$set':{'video.$.topComment': commentList}})
+        mycol.update_one({'video':{'$elemMatch':{'videoId': video['videoId']}}},{'$set':{'video.$.topComment': commentList}}) # we update the video with the comments
         print("comments added")
