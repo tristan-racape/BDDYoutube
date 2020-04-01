01f753b2ba9f73f24c4f4b00d4cacdfc61bb78b8
tristan-racape <62966441+tristan-racape@users.noreply.github.com>
2020-04-01 16:44:54 +0200

    update commentaires sur le code

--- insert_premier_dataset.py
+++ insert_premier_dataset.py
@@ -1,50 +1,55 @@
 import pymongo
 import json
 
+# parametrage mongodb
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["videos"]
 
-
+# lecture du fichier json initial de notre projet
 file = 'youtube_new/YouTubeDataset_withChannelElapsed.json'
 data = []
+
+# liste des atribbuts d'une chaine youtube (correspondant a certains champs dans le json)
+listattributechannel = ["channelViewCount", "subscriberCount","videoCount", "channelelapsedtime","channelCommentCount",
+                        "totvideos/videocount", "channelId", "totviews/totsubs", "totalviews/channelelapsedtime"]
+
+# liste des atribbuts d'une video youtube (correspondant a certains champs dans le json)
+listattributevideo = ["videoId", "videoCategoryId", "views/subscribers", "likes/subscriber", "likes/views",
+                      "dislikes/views", "videoViewCount", "comments/subscriber", "likes/dislikes", "comments/views",
+                      "elapsedtime", "videoDislikeCount", "dislikes/subscriber", "views/elapsedtime",
+                      "VideoCommentCount"]
+
+# ouverture du fichier json afin de pouvoir iterer a l'interieur
 with open(file) as f:
     for line in f:
         data.append(json.loads(line))
-
+        
+# pour chaque ligne on recupere les champs qui nous interessent et on les integre dans le dictionnaire correspodnant (channel ou video)
 for object in data[0]:
 
     channel = {}
 
     video = {}
 
-    for attribute,valeur in object.items():
-        if ((attribute == "channelViewCount") | (attribute =="subscriberCount") | (attribute =="videoCount") |
-                (attribute =="channelelapsedtime") | (attribute =="channelCommentCount") |
-                (attribute =="totvideos/videocount") | (attribute =="channelId") |
-                (attribute =="totviews/totsubs") | (attribute == "totalviews/channelelapsedtime")) :
-            channel[attribute] = valeur
-    # print(channel)
-    # ajouter a la db le dictionnaire channel
+    for attribute,value in object.items():
+        if attribute in listattributechannel :
+            channel[attribute] = value
+
+    # ajout a la database du dictionnaire channel
     x = mycol.insert_one(channel)
 
-    for attribute, valeur in object.items():
-        if ((attribute == "videoId") | (attribute == "videoCategoryId") | (attribute == "views/subscribers") | (
-                attribute == "likes/subscriber") | (attribute == "likes/views") | (
-                attribute == "dislikes/views") | (attribute == "videoViewCount") | (
-                attribute == "comments/subscriber") | (attribute == "likes/dislikes") | (attribute == "comments/views")
-                | (attribute == "elapsedtime") | (attribute == "videoDislikeCount") |
-                (attribute == "dislikes/subscriber")
-                | (attribute == "views/elapsedtime") | (attribute == "VideoCommentCount")):
-            video[attribute] = valeur
+    for attribute, value in object.items():
+        if attribute in listattributevideo:
+            video[attribute] = value
         if attribute == "channelId":
-            myquery = {attribute: valeur}
-        # do something for date isolation ?
+            myquery = {attribute: value}
         if attribute == "videoPublished":
-            myquery2 = {attribute: valeur}
-            video[attribute] = valeur
-    # print(video)
-    # PUSH le dictionnaire video
+            myquery2 = {attribute: value}
+            video[attribute] = value
+
+    # PUSH le dictionnaire video dans la database
+    # on utilise une requete pour mettre la video a l'interieur du channel dans le champ video
     newvideo = {"$push": {"video": video}}
     mycol.update_one(myquery, newvideo)
 
